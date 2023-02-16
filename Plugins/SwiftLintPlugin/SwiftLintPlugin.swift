cea0d30874544b79096a170a00984031dfd125a9
Masami Yamate <m.yamate@mail.yamatte.net>
2023-02-17 01:31:24 +0900

    wip

--- Plugins/SwiftLintPlugin/SwiftLintPlugin.swift
+++ Plugins/SwiftLintPlugin/SwiftLintPlugin.swift
@@ -11,18 +11,26 @@ import PackagePlugin
 struct SwiftLintPlugin: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
         return [
-            .prebuildCommand(displayName: "Run MyPlugin",
-                             executable: Path("/usr/bin/make"),
+                        .prebuildCommand(displayName: "Run MyPlugin",
+                             executable: Path("/bin/bash"),
                              arguments: [
-                                "hoge",
-                                "-C", "\(context.package.directory)",
-                                "WORKDIR=\(context.package.directory)",
-                                "FRAMEWORKNAME=XCDemo",
-                                "DST=\(context.pluginWorkDirectory.string)",
-                                "PATH=\(context.package.directory)",
-                                "DEMO=\(target.directory.string)"
+                                "build_tool.sh",
+                                "\(context.pluginWorkDirectory.string)"
                              ],
                              outputFilesDirectory: context.pluginWorkDirectory),
                 ]
+            // .prebuildCommand(displayName: "Run MyPlugin",
+            //                  executable: Path("/usr/bin/make"),
+            //                  arguments: [
+            //                     "hoge",
+            //                     "-C", "\(context.package.directory)",
+            //                     "WORKDIR=\(context.package.directory)",
+            //                     "FRAMEWORKNAME=XCDemo",
+            //                     "DST=\(context.pluginWorkDirectory.string)",
+            //                     "PATH=\(context.package.directory)",
+            //                     "DEMO=\(target.directory.string)"
+            //                  ],
+            //                  outputFilesDirectory: context.pluginWorkDirectory),
+            //     ]
     }
 }
